484d8d8732f1cc157ee4764d193690c688c6afd2
Evgeniy <KworkEvgeniy@yandex.ru>
2023-07-05 19:49:13 +0300

    Советы JS. Массив перебирающие методы – reduce и reduceRight.

--- JavaScript/tips.js
+++ JavaScript/tips.js
@@ -45,17 +45,44 @@ array = array.filter(function (elem) {
 
 /* every() работает так, что если все элементы массива прошли проверку, то она возвращает true.
 Но если хотя бы один не прошёл, то возвращает false. */
-array = array.every(function (elem) {
+new_array = array.every(function (elem) {
   return elem > 0;
 });
 
 /* some() работает так, что если хотя бы один элемент прошёл проверку, то она возвращает true.
 Но если никто не прошёл, то возвращает false. */
-array = array.some(function (elem) {
+new_array = array.some(function (elem) {
   return elem > 0;
 });
 
 
 /* МАССИВ ПЕРЕБИРАЮЩИЕ МЕТОДЫ - REDUCE И REDUCERIGHT - https://www.youtube.com/watch?v=Wy4rgg2iWpw&list=PLNkWIWHIRwMHKLotIS_d-wyj00pg0AnUg&index=6 */
+
+
+/* Метод reduce() и reduceRight() перебирают все элементы массива.
+Они служат для сложения, вычитания и тд всех элементов массива.
+Также они принимают промежуточный параметр, в который будет записываться результат.
+Разница в них лишь в том, что reduce() перебирает параметры слева направо, 
+а reduceRight() справа налево. */
+
+new_array = array.reduce(function (sum, elem) {
+  return sum += elem; // Вернёт сумму всех элементов.
+});
+
+/* Если передать второй параметр в функцию reduce() или reduceRight(), то
+первый параметр callback функции будет равен этому значению.
+Если его не передавать, то первый параметр будет равен первому элементу массива. */
+new_array = array.reduce(function (sum, elem) {
+  return sum += elem; // Вернёт сумму всех элементов.
+}, 10);
+
+// Функции reduce() и reduceRight() можно принимать для переделывания вложенных массивов в один.
+let flattened = [[0, 1], [2, 3], [4, 5]]
+flattened = flattened.reduceRight(function (sum, elem) {
+  return sum.concat(elem); // Вернёт один массив со всеми элементами вложенных массивов.
+}, []);
+
+
+/* ФУНКЦИИ: ПСЕВДОМАССИВ ARGUMENTS - https://www.youtube.com/watch?v=ZSrk2_W9FmY&list=PLNkWIWHIRwMHKLotIS_d-wyj00pg0AnUg&index=7 */
 
 
