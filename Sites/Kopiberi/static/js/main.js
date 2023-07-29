0ffb9f10a304891d8bc601d57890a696ca073be9
Evgeniy <KworkEvgeniy@yandex.ru>
2023-07-30 01:03:43 +0300

    Kopiberi. Добавление кнопки поиска.

--- Sites/Kopiberi/static/js/main.js
+++ Sites/Kopiberi/static/js/main.js
@@ -187,3 +187,16 @@ function download__form_image(input) {
     photo__wrapper.appendChild(img);
   }
 }
+
+// Уменьшение текста при наведении на поиск.
+header__navigation = document.getElementById("header__navigation");
+search = document.getElementById("search");
+nav__socials = document.getElementById("nav__socials");
+eventsObj.addEvent(search, "mouseover", function () {
+  header__navigation.classList.add('navigation__search')
+  nav__socials.classList.add('socials__search')
+});
+eventsObj.addEvent(search, "mouseout", function () {
+  header__navigation.classList.remove('navigation__seach')
+  nav__socials.classList.remove('socials__search')
+})
