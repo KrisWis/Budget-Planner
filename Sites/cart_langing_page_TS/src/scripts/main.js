eddaa945bc175cd019a0df37acc65c9f65c7469b
Evgeniy <KworkEvgeniy@yandex.ru>
2023-11-21 22:05:06 +0300

    Тестовое задание для практики в TS - завершено.

--- Sites/cart_langing_page_TS/src/scripts/main.js
+++ Sites/cart_langing_page_TS/src/scripts/main.js
@@ -14,13 +14,23 @@ const cart__items = document.getElementById("cart__items");
 for (let buy_button of item__buy_buttons) {
     buy_button.addEventListener("click", function () {
         let item_id = buy_button.id;
-        let item_amount = Number(document.getElementById(`${item_id}--amount`).value);
-        let item_price = document.getElementById(`${item_id}--price`).textContent;
-        let item_img = document.getElementById(`${item_id}--img`).src;
-        let item_caption = document.getElementById(`${item_id}--caption`).textContent;
-        let item_desc = document.getElementById(`${item_id}--desc`).textContent;
-        if (!document.getElementById(`cart__${item_id}`)) {
-            let comment__request = `<div class="cart__item" id="cart__${item_id}">
+        /* Открытие кругового меню выбора при нажатии */
+        let item_select_menu = document.getElementById(`${item_id}--amount`);
+        let select_menu__overlay = document.getElementById("select_menu__overlay");
+        let body = document.querySelector("body");
+        item_select_menu.classList.add("select_menu--open");
+        select_menu__overlay.classList.add("overlay--open");
+        body.classList.add("inactive");
+        /* Нажатие при выборе количества */
+        const select_menu__options = document.querySelectorAll(".amount--select_menu__option");
+        function select_menu_func(option) {
+            let item_amount = Number(option.textContent);
+            let item_price = document.getElementById(`${item_id}--price`).textContent;
+            let item_img = document.getElementById(`${item_id}--img`).src;
+            let item_caption = document.getElementById(`${item_id}--caption`).textContent;
+            let item_desc = document.getElementById(`${item_id}--desc`).textContent;
+            if (!document.getElementById(`cart__${item_id}`)) {
+                let comment__request = `<div class="cart__item" id="cart__${item_id}">
 
                 <div class="cart__item__img">
                     <img src="${item_img}" alt="Предмет в корзине">
@@ -39,42 +49,56 @@ for (let buy_button of item__buy_buttons) {
         
                 <button class="cart__item__delete" id="${item_id}--delete">Удалить</button>
             </div>`;
-            cart__items.insertAdjacentHTML(`beforeend`, comment__request);
-            /* Увеличение количества товара */
-            const cart__item__amount__plus = document.getElementById(`amount--plus__${item_id}`);
-            cart__item__amount__plus.addEventListener("click", function () {
-                document.getElementById(`amount--number__${item_id}`).textContent = String(Number(document.getElementById(`amount--number__${item_id}`).textContent) + 1);
-                const cart__final_price = Number(document.getElementById("cart__final_price").textContent);
-                document.getElementById("cart__final_price").textContent = String(cart__final_price + (Number(item_price.slice(0, -1)) * 1000));
-            });
-            /* Уменьшение количества товара */
-            const cart__item__amount__minus = document.getElementById(`amount--minus__${item_id}`);
-            cart__item__amount__minus.addEventListener("click", function () {
-                let item_amount = Number(document.getElementById(`amount--number__${item_id}`).textContent);
-                if (item_amount - 1 >= 1) {
-                    document.getElementById(`amount--number__${item_id}`).textContent = String(item_amount - 1);
+                cart__items.insertAdjacentHTML(`beforeend`, comment__request);
+                /* Увеличение количества товара */
+                const cart__item__amount__plus = document.getElementById(`amount--plus__${item_id}`);
+                cart__item__amount__plus.addEventListener("click", function () {
+                    document.getElementById(`amount--number__${item_id}`).textContent = String(Number(document.getElementById(`amount--number__${item_id}`).textContent) + 1);
                     const cart__final_price = Number(document.getElementById("cart__final_price").textContent);
-                    document.getElementById("cart__final_price").textContent = String(cart__final_price - (Number(item_price.slice(0, -1)) * 1000));
-                }
-            });
-            /* Удаление товара */
-            const cart__item__delete = document.getElementById(`${item_id}--delete`);
-            cart__item__delete.addEventListener("click", function () {
-                const cart__item = document.getElementById(`cart__${item_id}`);
+                    document.getElementById("cart__final_price").textContent = String(cart__final_price + (Number(item_price.slice(0, -1)) * 1000));
+                });
+                /* Уменьшение количества товара */
+                const cart__item__amount__minus = document.getElementById(`amount--minus__${item_id}`);
+                cart__item__amount__minus.addEventListener("click", function () {
+                    let item_amount = Number(document.getElementById(`amount--number__${item_id}`).textContent);
+                    if (item_amount - 1 >= 1) {
+                        document.getElementById(`amount--number__${item_id}`).textContent = String(item_amount - 1);
+                        const cart__final_price = Number(document.getElementById("cart__final_price").textContent);
+                        document.getElementById("cart__final_price").textContent = String(cart__final_price - (Number(item_price.slice(0, -1)) * 1000));
+                    }
+                });
+                /* Удаление товара */
+                const cart__item__delete = document.getElementById(`${item_id}--delete`);
+                cart__item__delete.addEventListener("click", function () {
+                    const cart__item = document.getElementById(`cart__${item_id}`);
+                    const cart__final_price = Number(document.getElementById("cart__final_price").textContent);
+                    let item_amount = Number(document.getElementById(`amount--number__${item_id}`).textContent);
+                    document.getElementById("cart__final_price").textContent = String(cart__final_price - (Number(item_price.slice(0, -1)) * 1000 * item_amount));
+                    cart__items.removeChild(cart__item);
+                });
+                /* Изменение финальной цены товара */
                 const cart__final_price = Number(document.getElementById("cart__final_price").textContent);
-                let item_amount = Number(document.getElementById(`amount--number__${item_id}`).textContent);
-                document.getElementById("cart__final_price").textContent = String(cart__final_price - (Number(item_price.slice(0, -1)) * 1000 * item_amount));
-                cart__items.removeChild(cart__item);
-            });
-            /* Изменение финальной цены товара */
-            const cart__final_price = Number(document.getElementById("cart__final_price").textContent);
-            document.getElementById("cart__final_price").textContent = String(cart__final_price + (Number(item_price.slice(0, -1)) * 1000 * item_amount));
+                document.getElementById("cart__final_price").textContent = String(cart__final_price + (Number(item_price.slice(0, -1)) * 1000 * item_amount));
+            }
+            else {
+                document.getElementById(`amount--number__${item_id}`).textContent = String(Number(document.getElementById(`amount--number__${item_id}`).textContent) + item_amount);
+                const cart__final_price = Number(document.getElementById("cart__final_price").textContent);
+                document.getElementById("cart__final_price").textContent = String(cart__final_price + (Number(item_price.slice(0, -1)) * 1000 * item_amount));
+            }
+            body.classList.remove("inactive");
+            item_select_menu.classList.remove("select_menu--open");
+            select_menu__overlay.classList.remove("overlay--open");
         }
-        else {
-            document.getElementById(`amount--number__${item_id}`).textContent = String(Number(document.getElementById(`amount--number__${item_id}`).textContent) + item_amount);
-            const cart__final_price = Number(document.getElementById("cart__final_price").textContent);
-            document.getElementById("cart__final_price").textContent = String(cart__final_price + (Number(item_price.slice(0, -1)) * 1000 * item_amount));
+        for (let option of select_menu__options) {
+            let select_menu_function = select_menu_func.bind(null, option);
+            option.onclick = select_menu_function;
         }
     });
 }
+/* Убираем класс для прерывания анимаций при загрузке страницы */
+window.onload = function () {
+    setTimeout(() => {
+        cart__modal_wrapper.classList.remove("preload");
+    }, 500);
+};
 //# sourceMappingURL=main.js.map
--- Sites/cart_langing_page_TS/src/scripts/main.ts
+++ Sites/cart_langing_page_TS/src/scripts/main.ts
@@ -17,15 +17,28 @@ const cart__items: HTMLElement = document.getElementById("cart__items");
 for (let buy_button of item__buy_buttons) {
     buy_button.addEventListener("click", function (): void {
         let item_id: string = (buy_button as HTMLElement).id;
-        let item_amount: number = Number((document.getElementById(`${item_id}--amount`) as HTMLSelectElement).value);
-        let item_price: string = document.getElementById(`${item_id}--price`).textContent;
-        let item_img: string = (document.getElementById(`${item_id}--img`) as HTMLImageElement).src;
-        let item_caption: string = document.getElementById(`${item_id}--caption`).textContent;
-        let item_desc: string = document.getElementById(`${item_id}--desc`).textContent;
 
-        if (!document.getElementById(`cart__${item_id}`)) {
-            let comment__request: string =
-                `<div class="cart__item" id="cart__${item_id}">
+        /* Открытие кругового меню выбора при нажатии */
+        let item_select_menu: HTMLElement = document.getElementById(`${item_id}--amount`);
+        let select_menu__overlay: HTMLElement = document.getElementById("select_menu__overlay");
+        let body: HTMLElement = document.querySelector("body");
+        item_select_menu.classList.add("select_menu--open");
+        select_menu__overlay.classList.add("overlay--open");
+        body.classList.add("inactive");
+
+        /* Нажатие при выборе количества */
+        const select_menu__options: NodeListOf<Node> = document.querySelectorAll(".amount--select_menu__option");
+
+        function select_menu_func(option) {
+            let item_amount: number = Number(option.textContent);
+            let item_price: string = document.getElementById(`${item_id}--price`).textContent;
+            let item_img: string = (document.getElementById(`${item_id}--img`) as HTMLImageElement).src;
+            let item_caption: string = document.getElementById(`${item_id}--caption`).textContent;
+            let item_desc: string = document.getElementById(`${item_id}--desc`).textContent;
+
+            if (!document.getElementById(`cart__${item_id}`)) {
+                let comment__request: string =
+                    `<div class="cart__item" id="cart__${item_id}">
 
                 <div class="cart__item__img">
                     <img src="${item_img}" alt="Предмет в корзине">
@@ -44,48 +57,65 @@ for (let buy_button of item__buy_buttons) {
         
                 <button class="cart__item__delete" id="${item_id}--delete">Удалить</button>
             </div>`;
-            cart__items.insertAdjacentHTML(`beforeend`,
-                comment__request
-            );
+                cart__items.insertAdjacentHTML(`beforeend`,
+                    comment__request
+                );
 
-            /* Увеличение количества товара */
-            const cart__item__amount__plus: HTMLElement = document.getElementById(`amount--plus__${item_id}`);
+                /* Увеличение количества товара */
+                const cart__item__amount__plus: HTMLElement = document.getElementById(`amount--plus__${item_id}`);
 
-            cart__item__amount__plus.addEventListener("click", function (): void {
-                document.getElementById(`amount--number__${item_id}`).textContent = String(Number(document.getElementById(`amount--number__${item_id}`).textContent) + 1);
-                const cart__final_price: number = Number(document.getElementById("cart__final_price").textContent);
-                document.getElementById("cart__final_price").textContent = String(cart__final_price + (Number(item_price.slice(0, -1)) * 1000));
-            })
-
-            /* Уменьшение количества товара */
-            const cart__item__amount__minus: HTMLElement = document.getElementById(`amount--minus__${item_id}`);
-
-            cart__item__amount__minus.addEventListener("click", function (): void {
-                let item_amount: number = Number(document.getElementById(`amount--number__${item_id}`).textContent);
-                if (item_amount - 1 >= 1) {
-                    document.getElementById(`amount--number__${item_id}`).textContent = String(item_amount - 1);
+                cart__item__amount__plus.addEventListener("click", function (): void {
+                    document.getElementById(`amount--number__${item_id}`).textContent = String(Number(document.getElementById(`amount--number__${item_id}`).textContent) + 1);
                     const cart__final_price: number = Number(document.getElementById("cart__final_price").textContent);
-                    document.getElementById("cart__final_price").textContent = String(cart__final_price - (Number(item_price.slice(0, -1)) * 1000));
-                }
-            })
+                    document.getElementById("cart__final_price").textContent = String(cart__final_price + (Number(item_price.slice(0, -1)) * 1000));
+                })
 
-            /* Удаление товара */
-            const cart__item__delete: HTMLElement = document.getElementById(`${item_id}--delete`);
-            cart__item__delete.addEventListener("click", function (): void {
-                const cart__item: HTMLElement = document.getElementById(`cart__${item_id}`);
+                /* Уменьшение количества товара */
+                const cart__item__amount__minus: HTMLElement = document.getElementById(`amount--minus__${item_id}`);
+
+                cart__item__amount__minus.addEventListener("click", function (): void {
+                    let item_amount: number = Number(document.getElementById(`amount--number__${item_id}`).textContent);
+                    if (item_amount - 1 >= 1) {
+                        document.getElementById(`amount--number__${item_id}`).textContent = String(item_amount - 1);
+                        const cart__final_price: number = Number(document.getElementById("cart__final_price").textContent);
+                        document.getElementById("cart__final_price").textContent = String(cart__final_price - (Number(item_price.slice(0, -1)) * 1000));
+                    }
+                })
+
+                /* Удаление товара */
+                const cart__item__delete: HTMLElement = document.getElementById(`${item_id}--delete`);
+                cart__item__delete.addEventListener("click", function (): void {
+                    const cart__item: HTMLElement = document.getElementById(`cart__${item_id}`);
+                    const cart__final_price: number = Number(document.getElementById("cart__final_price").textContent);
+                    let item_amount: number = Number(document.getElementById(`amount--number__${item_id}`).textContent);
+                    document.getElementById("cart__final_price").textContent = String(cart__final_price - (Number(item_price.slice(0, -1)) * 1000 * item_amount));
+                    cart__items.removeChild(cart__item);
+                })
+
+                /* Изменение финальной цены товара */
                 const cart__final_price: number = Number(document.getElementById("cart__final_price").textContent);
-                let item_amount: number = Number(document.getElementById(`amount--number__${item_id}`).textContent);
-                document.getElementById("cart__final_price").textContent = String(cart__final_price - (Number(item_price.slice(0, -1)) * 1000 * item_amount));
-                cart__items.removeChild(cart__item);
-            })
+                document.getElementById("cart__final_price").textContent = String(cart__final_price + (Number(item_price.slice(0, -1)) * 1000 * item_amount));
+            } else {
+                document.getElementById(`amount--number__${item_id}`).textContent = String(Number(document.getElementById(`amount--number__${item_id}`).textContent) + item_amount);
+                const cart__final_price: number = Number(document.getElementById("cart__final_price").textContent);
+                document.getElementById("cart__final_price").textContent = String(cart__final_price + (Number(item_price.slice(0, -1)) * 1000 * item_amount));
+            }
 
-            /* Изменение финальной цены товара */
-            const cart__final_price: number = Number(document.getElementById("cart__final_price").textContent);
-            document.getElementById("cart__final_price").textContent = String(cart__final_price + (Number(item_price.slice(0, -1)) * 1000 * item_amount));
-        } else {
-            document.getElementById(`amount--number__${item_id}`).textContent = String(Number(document.getElementById(`amount--number__${item_id}`).textContent) + item_amount);
-            const cart__final_price: number = Number(document.getElementById("cart__final_price").textContent);
-            document.getElementById("cart__final_price").textContent = String(cart__final_price + (Number(item_price.slice(0, -1)) * 1000 * item_amount));
+            body.classList.remove("inactive");
+            item_select_menu.classList.remove("select_menu--open");
+            select_menu__overlay.classList.remove("overlay--open");
+        }
+
+        for (let option of select_menu__options) {
+            let select_menu_function: VoidFunction = select_menu_func.bind(null, option);
+            (option as HTMLElement).onclick = select_menu_function;
         }
     })
+}
+
+/* Убираем класс для прерывания анимаций при загрузке страницы */
+window.onload = function () {
+    setTimeout(() => {
+        cart__modal_wrapper.classList.remove("preload");
+    }, 500);
 }
