ac98dde28cfbc96f721470a6a254c9bf981c857c
Evgeniy <KworkEvgeniy@yandex.ru>
2023-11-24 18:24:01 +0300

    Использование Vite для лендинга практики в TS и Questionnaire. Также, изучен ещё один урок по Angular.

--- JavaScript/Angular/angular-ekart/src/app/app.component.ts
+++ JavaScript/Angular/angular-ekart/src/app/app.component.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+// Декоратор компонента
 @Component({
+  // Определяем селектор (app-root используется в index.html)
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
+  // В templateUrl подключаем файл нашего html компонента, который будет использоваться
   templateUrl: './app.component.html',
+  // В styleURL подключаем файл компонента стилей, который будет использоваться
   styleUrl: './app.component.scss'
 })
+
+// Создаём класс компонента нашего приложения. Из него мы можем брать различные свойства для использования в app.component.html.
 export class AppComponent {
   title = 'angular-ekart';
 }
---
+++ Sites/cart_landing_page_TS/vite.config.ts
@@ -0,0 +1,16 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import { ViteMinifyPlugin } from 'vite-plugin-minify'
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                file1: resolve(__dirname, 'index.html')
+            }
+        }
+    },
+    plugins: [
+        ViteMinifyPlugin({}),
+    ]
+})
