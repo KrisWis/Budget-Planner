0c2625bc55abebf48e551aec46002e42471d0340
Evgeniy <KworkEvgeniy@yandex.ru>
2024-01-14 23:54:57 +0300

    React Pizza v2 - курс начат.

---
+++ JavaScript/React/pizza.js
@@ -0,0 +1,15 @@
+
+
+/* REACT PIZZA V2 [REMASTERED] - https://www.youtube.com/playlist?list=PL0FGkDGJQjJG9eI85xM1_iLIf6BcEdaNl */
+
+
+/* ДЛЯ КОГО ДАННЫЙ КУРС И КАК ЕГО СМОТРЕТЬ? ЧТО НОВОГО? - https://www.youtube.com/watch?v=_UywBskWJ7Q&list=PL0FGkDGJQjJG9eI85xM1_iLIf6BcEdaNl */
+
+
+
+
+
+/* РАЗРАБАТЫВАЕМ ЛУЧШУЮ ПИЦЦЕРИЮ НА REACT + TYPESCRIPT + REDUX TOOLKIT + ROUTER V6 - https://www.youtube.com/watch?v=okqptJNYbXg&list=PL0FGkDGJQjJG9eI85xM1_iLIf6BcEdaNl&index=2 */
+
+
+// Redux Toolkit - это типа бд, но на фронтенде.
