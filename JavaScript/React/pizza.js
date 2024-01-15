8d3f3a6582b20270d306b032fc3d2361ca21e165
Evgeniy <KworkEvgeniy@yandex.ru>
2024-01-15 20:15:11 +0300

    🍕 React Pizza v2 — разрабатываем лучшую пиццерию на React + TypeScript + Redux Toolkit + Router v6 - урок пройден.

--- JavaScript/React/pizza.js
+++ JavaScript/React/pizza.js
@@ -6,10 +6,17 @@
 /* ДЛЯ КОГО ДАННЫЙ КУРС И КАК ЕГО СМОТРЕТЬ? ЧТО НОВОГО? - https://www.youtube.com/watch?v=_UywBskWJ7Q&list=PL0FGkDGJQjJG9eI85xM1_iLIf6BcEdaNl */
 
 
-
+// Ну крч, мне подходит :)
 
 
 /* РАЗРАБАТЫВАЕМ ЛУЧШУЮ ПИЦЦЕРИЮ НА REACT + TYPESCRIPT + REDUX TOOLKIT + ROUTER V6 - https://www.youtube.com/watch?v=okqptJNYbXg&list=PL0FGkDGJQjJG9eI85xM1_iLIf6BcEdaNl&index=2 */
 
 
 // Redux Toolkit - это типа бд, но на фронтенде.
+
+// Функция считается функциональным компонентом, если она возвращает JSX разметку.
+
+
+/* СОЗДАЁМ КОМПОНЕНТ, SCSS, IMPORT/EXPORT, PROPS (ПРОПСЫ) - https://www.youtube.com/watch?v=HAMGGnF7en4&list=PL0FGkDGJQjJG9eI85xM1_iLIf6BcEdaNl&index=3 */
+
+
